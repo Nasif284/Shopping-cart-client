c1e4ca34123d77c5acc82789c22da25f44797569
Muhammad Nasif <nasifhima@gmail.com>
2025-10-30 19:36:05 +0530

    pwa feature added

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,43 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate", 
+      // includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
+      manifest: {
+        name: "My App",
+        short_name: "App",
+        description: "A sample progressive web app built with React + Vite",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+        start_url: "/",
+        // icons: [
+        //   {
+        //     src: "/pwa-192x192.png",
+        //     sizes: "192x192",
+        //     type: "image/png",
+        //   },
+        //   {
+        //     src: "/pwa-512x512.png",
+        //     sizes: "512x512",
+        //     type: "image/png",
+        //   },
+        //   {
+        //     src: "/pwa-512x512.png",
+        //     sizes: "512x512",
+        //     type: "image/png",
+        //     purpose: "any maskable",
+        //   },
+        // ],
+      },
+    }),
+  ],
 });
